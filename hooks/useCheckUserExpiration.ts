956c3a4f93c83c67b62e5cb2e5107165696f8cf5
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2025-02-11 17:28:36 +0200

    refactor: format files

--- hooks/useCheckUserExpiration.ts
+++ hooks/useCheckUserExpiration.ts
@@ -3,15 +3,15 @@ import { useEffect } from "react";
 import useUserStore from "../zustand/userStore";
 
 const useCheckUserExpiration = () => {
- const { user, expiresAt, clearUser } = useUserStore();
+  const { user, expiresAt, clearUser } = useUserStore();
 
- useEffect(() => {
-  if (typeof window !== "undefined") {
-   if (user && expiresAt && Date.now() > expiresAt) {
-    clearUser();
-   }
-  }
- }, [user, expiresAt, clearUser]);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (user && expiresAt && Date.now() > expiresAt) {
+        clearUser();
+      }
+    }
+  }, [user, expiresAt, clearUser]);
 };
 
 export default useCheckUserExpiration;
