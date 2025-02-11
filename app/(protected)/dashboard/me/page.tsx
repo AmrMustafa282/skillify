51f67d447255862637ac91f3631754abdd989db5
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2025-02-11 17:23:35 +0200

    feat: add (portected) layout

--- app/(protected)/dashboard/me/page.tsx
+++
@@ -1,6 +0,0 @@
-const Me = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
- return <div>Me</div>;
-};
-
-export default Me;
--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -3,7 +3,18 @@ import { useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Loader from "@/components/ui/Loader";
-import Header from "@/components/Header";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { Separator } from "@/components/ui/separator";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { ModeToggle } from "@/components/ui/toggle-theme";
 
 export default function MePage({ children }: { children: React.ReactNode }) {
   const { data: session, status } = useSession();
@@ -18,11 +29,35 @@ export default function MePage({ children }: { children: React.ReactNode }) {
   if (status === "loading") return <Loader />;
   if (!session) return null;
 
+  //@ts-expect-error
+  const role = session?.user?.role;
 
   return (
-   <main className="flex flex-col  h-screen">
-     <Header />
-     <div className="flex-grow container mx-auto">{children}</div>
-   </main>
+    <SidebarProvider>
+      <AppSidebar role={role} />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+          <div className="ml-auto mr-4">
+            <ModeToggle />
+          </div>
+        </header>
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
