df431f221eb48f05aaed8819d74c4b5ee4f635e1
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2025-02-11 17:27:06 +0200

    refactor: format files

--- zustand/userStore.ts
+++ zustand/userStore.ts
@@ -3,34 +3,34 @@ import { persist, createJSONStorage } from "zustand/middleware";
 import { devtools } from "zustand/middleware";
 
 export interface User {
- id: string;
- name: string;
- email: string;
- profilePicture?: string;
+  id: string;
+  name: string;
+  email: string;
+  profilePicture?: string;
 }
 
 export interface UserStoreState {
- user: User | null;
- expiresAt: number | null;
- setUser: (user: User | null, expiresAt: number | null) => void;
- clearUser: () => void;
+  user: User | null;
+  expiresAt: number | null;
+  setUser: (user: User | null, expiresAt: number | null) => void;
+  clearUser: () => void;
 }
 
 const useUserStore = create<UserStoreState>()(
- devtools(
-  persist(
-   (set) => ({
-    user: null,
-    expiresAt: null,
-    setUser: (user, expiresAt) => set({ user, expiresAt }),
-    clearUser: () => set({ user: null, expiresAt: null }),
-   }),
-   {
-    name: "user-storage", // Key for localStorage
-    storage: createJSONStorage(() => localStorage), // Persist to localStorage
-   }
+  devtools(
+    persist(
+      (set) => ({
+        user: null,
+        expiresAt: null,
+        setUser: (user, expiresAt) => set({ user, expiresAt }),
+        clearUser: () => set({ user: null, expiresAt: null }),
+      }),
+      {
+        name: "user-storage", // Key for localStorage
+        storage: createJSONStorage(() => localStorage), // Persist to localStorage
+      }
+    )
   )
- )
 );
 
 export default useUserStore;
